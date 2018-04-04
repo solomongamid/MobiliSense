7d64e92b2aa2fcb2fb4c575c881b5a503849fd48
Raphael Renout <raphael.renout@gmail.com>
2018-04-04 18:20:28 +0200

    footer v1

--- resources/views/benevole.blade.php
+++ resources/views/benevole.blade.php
@@ -1,77 +1,33 @@
-<!doctype html>
-<html lang="{{ app()->getLocale() }}">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('mainbenevole')
 
-        <title>MobiliSense</title>
+@section('title', 'Mon espace')
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
 
-        <!-- Style -->
-        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-        <link href="{{ asset('css/main.css') }}" rel="stylesheet">
-        <link href="{{ asset('css/epe.css') }}" rel="stylesheet">
 
-    </head>
-    <body>
-
-      <nav id="mainNav" class="navbar navbar-default navbar-fixed-top navbar-custom">
-        <div class="containerfluid">
-            <!-- Brand and toggle get grouped for better mobile display -->
-            <div class="navbar-header page-scroll">
-                <a class="navbar-brand" href="#page-top">MobiliSense</a>
-            </div>
-
-            <!-- Collect the nav links, forms, and other content for toggling -->
-            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-                <ul class="nav navbar-nav">
-                    <li class="hidden">
-                        <a href="#page-top"></a>
-                    </li>
-                    <li class="page-scroll dropdown">
-                        <a href="#portfolio" class="dropbtn">Accueil</a>
-                    </li>
-                    <li class="page-scroll dropdown">
-                      <a href="#portfolio" class="dropbtn">Bilan</a>
-                    </li>
-                    <li class="page-scroll dropdown">
-                      <a href="#portfolio" class="dropbtn">Véritas</a>
-                    </li>
-                </ul>
-                @if (Auth::guest())
-                    <a type="button" class="navbar-btn" href="{{ route('login') }}">Login</a>
-                @else
-                    <a type="button" class="navbar-btn" href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
-                      Logout
-                    </a>
-                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                        {{ csrf_field() }}
-                    </form>
-                @endif
-            </div>
-        </div>
-      </nav>
+@section('content')
 
       <main class="mainuserhp">
 
         <!-- Header -->
         <div class="row">
-          <div class="col-xs-4 center-left">
+          <div class="col-xs-2">
+          </div>
+          <div class="col-xs-5 center-left">
             <h1>Vague 1</h1>
             <h3 >Jour 1</h3>
           </div>
-          <div class="col-xs-6">
+          <div class="col-xs-3">
             <h1>Date</h1>
             <h3>Le 09 Mars 2018</h3>
           </div>
         </div>
 
         <div class="row">
-          <div class="col-xs-5">
-            <hr>
+          <div class="col-xs-2 day">
+            <img class="moon" src="{{ asset('img/sun.png') }}">
+          </div>
+          <div class="col-xs-3">
+            <hr class="hrday">
             <img height="500px;" src="{{ asset('img/humanbody.png') }}"/>
             <a href="#">
               <img class="circle1" src="{{ asset('img/circle.png') }}" />
@@ -90,14 +46,20 @@
               </span>
             </a>
           </div>
-          <div class="col-xs-4">
-            <img class="moon" src="{{ asset('img/sun.png') }}">
-            <ul class="infoepe">
-              <li>Passez votre souris sur un des ronds pour voir le détail d'un des capteurs à porter aujourd'hui</li>
-              <li>N'hésitez pas à cliquez sur le lien en savoir plus pour avoir plus de détails</li>
-              <li>N'oubliez pas de mettre les capteurs demain à charger afin d'être le mieux équipé possible</li>
-              <li>N'hésitez pas à nous contacter pour toute question / soucis technique</li>
+          <div class="col-xs-5 infoepe">
+            <h3>Matin :</h3>
+            <p>Réalisez 3 mesures...</p>
+            <h3>Journée :</h3>
+            <p>Portez du lever au coucher...</p>
+            <ul class="listinfoepe">
+              <li>Capteur 1</li>
+              <li>Capteur 4</li>
+              <li>Capteur 6</li>
             </ul>
+            <p>Remplissez votre carnet de déplacement</p>
+            <p>Répondez au questionnaire sur l'Iphone</p>
+            <h3>Soir :</h3>
+            <p>Réaliser 3 mesures...</p>
           </div>
           <div class="col-xs-2">
             <img class="timev1" src="{{ asset('img/flecheright.png') }}" />
@@ -105,9 +67,12 @@
           </div>
         </div>
 
-        <div class="row">
+        <div class="row night">
+          <div class="col-xs-2 day">
+            <img class="moon" src="{{ asset('img/moon.png') }}" />
+          </div>
           <div class="col-xs-5">
-            <hr>
+            <hr class="hrnight">
             <h3 class="center-left">A recharger :</h3>
             <ul class="center-left">
               <li>Capteur 3</li>
@@ -115,41 +80,7 @@
               <li>Capteur 6</li>
             </ul>
           </div>
-          <div class="col-xs-4">
-            <img class="moon" src="{{ asset('img/moon.png') }}" />
-          </div>
+
         </div>
       </main>
-      <!-- Footer -->
-      <footer class="text-center">
-          <div class="footer-above">
-              <div class="container">
-                  <div class="row">
-                      <div class="footer-col col-md-4">
-                        <h2>Liens utiles</h2>
-                        <ul class="list-footer">
-                          <li>
-                            <a href="#">Equipe</a>
-                          </li>
-                          <li>
-                            <a href="#">Contact</a>
-                          </li>
-                        </ul>
-                      </div>
-                      <div class="footer-col col-md-4">
-                        <img class="footer-img" src="./img/logo_inserm.png" alt="footerLogo">
-                      </div>
-                      <div class="footer-col col-md-4">
-                        <h2>Remerciements</h2>
-                        <ul class="thanks-footer">
-                          <li>Raphael RENOIT</li>
-                          <li>Suliman ABDELGADEIR</li>
-                          <li>Qixin YING</li>
-                        </ul>
-                      </div>
-                  </div>
-              </div>
-          </div>
-      </footer>
-    </body>
-</html>
+@endsection
---
+++ resources/views/mainbenevole.blade.php
@@ -0,0 +1,96 @@
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>MobiliSense - @yield('title')</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+
+        <!-- Style -->
+        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+        <link href="{{ asset('css/main.css') }}" rel="stylesheet">
+        <link href="{{ asset('css/epe.css') }}" rel="stylesheet">
+
+        <script src="./js/app.js"></script>
+
+    </head>
+    <body>
+
+      <nav id="mainNav" class="navbar navbar-default navbar-fixed-top navbar-custom">
+        <div class="containerfluid">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header page-scroll">
+                <a class="navbar-brand" href="#page-top">MobiliSense</a>
+            </div>
+
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav">
+                    <li class="hidden">
+                        <a href="/"></a>
+                    </li>
+                    <li class="page-scroll dropdown">
+                        <a href="/" class="dropbtn">Accueil</a>
+                    </li>
+                    <li class="page-scroll dropdown">
+                        <a href="#benevole" class="dropbtn">Mon Espace</a>
+                    </li>
+                    <li class="page-scroll dropdown">
+                      <a href="#bilan" class="dropbtn">Mon Bilan</a>
+                    </li>
+                    <li class="page-scroll dropdown">
+                      <a href="#questionnaire" class="dropbtn">Questionnaire Géo</a>
+                    </li>
+                </ul>
+                @if (Auth::guest())
+                    <a type="button" class="navbar-btn" href="{{ route('login') }}">Login</a>
+                @else
+                    <a type="button" class="navbar-btn" href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+                      Logout
+                    </a>
+                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                        {{ csrf_field() }}
+                    </form>
+                @endif
+            </div>
+        </div>
+      </nav>
+
+      @yield('content')
+
+    <!-- Footer -->
+    <footer class="text-center">
+                <div class="row">
+                    <div class="footer-col col-md-2">
+                      <h2>MobiliSense</h2>
+                      <p>
+                          Inserm IPLESP Unité 1136 - Equipe 8 Nemesis<br>
+                          Faculté de médecine - Hôpital St-Antoine<br>
+                          27, rue Chaligny 75012 Paris
+                      </p>
+                    </div>
+                    <div class="footer-col col-md-6">
+                      <img class="footer-img" src="./img/iple.png" alt="Institut Pierre Louis">
+                      <img class="footer-img" src="./img/logo_inserm.png" alt="Inserm"><br>
+                      <img class="footer-img" src="./img/erc.jpg" alt="European Research Council">
+                      <img class="footer-img" src="./img/sorbonne.png" alt="footerLogo">
+                      <img class="footer-img" src="./img/france.jpg" alt="footerLogo">
+                    </div>
+                    <div class="footer-col col-md-2">
+                      <h2>Remerciements</h2>
+                      <p>
+                        Merci à toutes les personnes volontaires qui, par leur participation, contribuent au succès de cette recherche en santé publique.
+                      </p>
+                    </div>
+                </div>
+        <div class="row ml">
+          <p>Mentions légales</p>
+          <p>Merci à R. Renout, Y. Qixin, S. Abul, C. Rabehi qui ont développé ce site</p>
+        </div>
+    </footer>
+  </body>
+</html>
